cbf8818990413aeb577d66e0a5da3e7032ff4a95
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-11-06 00:16:41 -0500

    Added triangle query benchmark

---
+++ benches/triangle.rs
@@ -0,0 +1,40 @@
+#![feature(proc_macro)]
+#![feature(test)]
+extern crate mycroft;
+extern crate mycroft_macros;
+extern crate mycroft_support;
+extern crate test;
+extern crate rand;
+
+use mycroft_macros::mycroft_program;
+use test::Bencher;
+
+mycroft_program!("
+P(usize, usize)
+Q(usize, usize)
+R(usize, usize)
+?Triangle: P(x, y) & Q(y, z) & R(z, x)
+");
+
+fn triangle_n(n: usize, b: &mut Bencher) {
+    use mycroft_program::{Database, P, Q, R};
+    let mut db = Database::new();
+    for _ in 0..n {
+        db.insert_p(P { arg0: rand::random(), arg1: rand::random() });
+        db.insert_q(Q { arg0: rand::random(), arg1: rand::random() });
+        db.insert_r(R { arg0: rand::random(), arg1: rand::random() });
+    }
+    b.iter(|| {
+        test::black_box(db.query_triangle())
+    })
+}
+
+#[bench]
+fn triangle_1k(b: &mut Bencher) {
+    triangle_n(1_000, b)
+}
+
+#[bench]
+fn triangle_10k(b: &mut Bencher) {
+    triangle_n(10_000, b)
+}
